89e9ce89c26bb40e6a8ec39bf0bec1ea125842f9
Arnab Mitra <arnabmitra471@gmail.com>
2023-11-28 08:32:23 +0530

    Changed the color of the heading and added an underline

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -35,7 +35,7 @@ function Users() {
     }
   return (
     <div className='container'>
-        <h2 className='w-100 d-flex justify-content-center p-3'>Employee management system</h2>
+        <h2 className='w-100 d-flex justify-content-center p-3 text-decoration-underline text-primary'>Employee management system</h2>
         <div className='row'>
             <div className="col-md-12">
                 <p><Link to="/add" className="btn btn-success">Add new users</Link></p>
